ce74837533770d246b08240733818bca3ff68c86
Samy Hamed-e-saberi <samsam92izii@gmail.com>
2023-03-15 14:19:49 +0100

    images module

--- green/nuxt.config.ts
+++ green/nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
     '@/assets/css/Page-2.css',
   ],
   modules: [
-    '@nuxtjs/tailwindcss'
-  ]
+    '@nuxt/image-edge'
+  ],
+  image: {
+    // The screen sizes predefined by `@nuxt/image`:
+    screens: {
+      xs: 320,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      xxl: 1536,
+      '2xl': 1536
+    },
+  }
 })
