ea9ed1275cd6ee924afa70f5f135a240a02e39dc
Théo Sigaud <sigaudtheo@gmail.com>
2023-03-17 16:30:58 +0100

    Change baslie h4

--- green/components/Home.vue
+++ green/components/Home.vue
@@ -4,8 +4,8 @@
     <section class="u-align-center u-clearfix u-image u-section-1" id="carousel_bd75">
       <div class="u-clearfix u-sheet u-sheet-1">
         <h1 class="u-align-center-xs u-custom-font u-font-lato u-text u-text-body-alt-color u-title u-text-1">xc60</h1>
-        <h4 class="u-align-center-xs u-custom-font u-text u-text-body-alt-color u-text-font u-text-2">A safe way to move
-          a family around in style.</h4>
+        <p class="u-align-center-xs u-custom-font u-text u-text-body-alt-color u-text-font u-text-2">A safe way to move
+          a family around in style.</p>
       </div>
     </section>
     <section class="u-clearfix u-gradient u-section-2" id="carousel_af51">
