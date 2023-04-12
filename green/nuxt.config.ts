136efaab3076e7892768023e600483c35b145bec
Sadik Raida <sadik.raida@gmail.com>
2023-04-12 18:40:04 +0200

    maj config

--- green/nuxt.config.ts
+++ green/nuxt.config.ts
@@ -50,8 +50,8 @@ export default defineNuxtConfig({
       ],
       script: [
         {
-          src: 'https://code.jquery.com/jquery-3.3.1.slim.min.js',
-          integrity: 'sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo',
+          src: 'https://code.jquery.com/jquery-3.6.4.js',
+          integrity: 'sha256-a9jBBRygX1Bh5lt8GZjXDzyOB+bWve9EiO7tROUtj/E=',
           crossorigin: 'anonymous',
         },
         {
