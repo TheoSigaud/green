d89075ef72b605ac42c2b3e33362fc39867d2766
Sadik Raida <sadik.raida@gmail.com>
2023-04-12 15:08:14 +0200

    fix build

--- green/pages/cars.vue
+++ green/pages/cars.vue
@@ -23,7 +23,7 @@
         </div>
     </header>
 
-    <section class="section section-bg" id="call-to-action" style="background-image: url('/banner-image-1-1920x500.jpg')">
+    <section class="section section-bg" id="call-to-action" style="background-image: url('/banner-image-1-1920x500.webp')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
