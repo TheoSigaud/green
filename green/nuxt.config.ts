6cc4a81c2f96973330bd29f4df3f4caf1b8fa2b8
Théo Sigaud <sigaudtheo@gmail.com>
2023-04-13 00:01:15 +0200

    Local min js

--- green/nuxt.config.ts
+++ green/nuxt.config.ts
@@ -6,6 +6,9 @@ export default defineNuxtConfig({
     '@/assets/css/style.css',
     '@/assets/css/bootstrap.min.css',
   ],
+  script: [
+      '@/assets/js/bootstrap.min.js',
+  ],
   modules: [
     'nuxt-purgecss',
     ['@nuxtjs/robots', { configPath: "~/robots.config" }],
@@ -50,11 +53,6 @@ export default defineNuxtConfig({
         {
           src: 'https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js',
           integrity: 'sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1',
-          crossorigin: 'anonymous',
-        },
-        {
-          src: 'https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js',
-          integrity: 'sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM',
           crossorigin: 'anonymous',
         },
       ],
