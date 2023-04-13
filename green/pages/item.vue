cb4d5c3aa085ac8f8869d012a209eee88372b051
Théo Sigaud <sigaudtheo@gmail.com>
2023-04-14 00:26:36 +0200

    Vue 3

--- green/pages/item.vue
+++ green/pages/item.vue
@@ -1,35 +1,28 @@
-<script>
-import { Splide, SplideSlide } from '@splidejs/vue-splide';
+<script setup>
+import {Splide, SplideSlide} from '@splidejs/vue-splide';
 
-export default defineComponent( {
-  components: {
-    Splide,
-    SplideSlide,
-  },
+const options = {
+    rewind: true,
+    autoWidth: true,
 
-  setup() {
-    const options = {
-      rewind: true,
-      autoWidth: true,
-    };
-
-    return { options };
-  },
-} );
+}
 </script>
 
 
 <template>
-    <section class="section section-bg" id="call-to-action" style="background-image: url('/banner-image-1-1920x500.webp')">
+    <section class="section section-bg" id="call-to-action"
+             style="background-image: url('/banner-image-1-1920x500.webp')">
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
                     <div class="cta-content">
                         <br>
                         <br>
-                        <h2><small><del>$12 999</del></small> <em>$11 779</em></h2>
+                        <h2><small>
+                            <del>$12 999</del>
+                        </small> <em>$11 779</em></h2>
                         <p>Roulez à bord de la meilleur voiture du marché.</p>
-                        <button type="button" class="btn " id="btnPaiement" >Paiement</button>
+                        <button type="button" class="btn " id="btnPaiement">Paiement</button>
                     </div>
                 </div>
             </div>
@@ -42,163 +35,175 @@ export default defineComponent( {
             <br>
 
             <Splide :options="options" aria-label="Carousel">
-    <SplideSlide>
-      <nuxt-img src="/1.webp" style="width: 1200px" alt="Angle 1"/>
-    </SplideSlide>
-    <SplideSlide>
-      <nuxt-img src="/2.webp" style="width: 1200px" alt="Angle 2"/>
-    </SplideSlide>
-    <SplideSlide>
-      <nuxt-img src="/3.webp" style="width: 1200px" alt="Angle 3"/>
-    </SplideSlide>
-    <SplideSlide>
-      <nuxt-img src="/4.webp" style="width: 1200px" alt="Angle 4"/>
-    </SplideSlide>
-    <SplideSlide>
-      <nuxt-img src="/5.webp" style="width: 1200px" alt="Angle 5"/>
-    </SplideSlide>
-  </Splide>
-            
+                <SplideSlide>
+                    <nuxt-img src="/1.webp" style="width: 1200px" alt="Angle 1"/>
+                </SplideSlide>
+                <SplideSlide>
+                    <nuxt-img src="/2.webp" style="width: 1200px" alt="Angle 2"/>
+                </SplideSlide>
+                <SplideSlide>
+                    <nuxt-img src="/3.webp" style="width: 1200px" alt="Angle 3"/>
+                </SplideSlide>
+                <SplideSlide>
+                    <nuxt-img src="/4.webp" style="width: 1200px" alt="Angle 4"/>
+                </SplideSlide>
+                <SplideSlide>
+                    <nuxt-img src="/5.webp" style="width: 1200px" alt="Angle 5"/>
+                </SplideSlide>
+            </Splide>
+
             <br>
             <br>
 
-            
+
             <div class="row" id="tabs">
-              <div class="col-lg-12">
-                <section class="tab-content" id="v-pills-tabContent" style="width: 100%;">
-                  <article  class="tab-pane fade show active" id="v-pills-un" role="tabpanel" aria-labelledby="v-pills-un-tab">
-                    <h3>Vehicle Specs</h3>
+                <div class="col-lg-12">
+                    <section class="tab-content" id="v-pills-tabContent" style="width: 100%;">
+                        <article class="tab-pane fade show active" id="v-pills-un" role="tabpanel"
+                                 aria-labelledby="v-pills-un-tab">
+                            <h3>Vehicle Specs</h3>
 
-                    <div class="row">
-                       <div class="col-sm-6">
-                            <label>Type</label>
-                       
-                            <p>Used vehicle</p>
-                       </div>
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label>Type</label>
 
-                       <div class="col-sm-6">
-                            <label>Make</label>
-                       
-                            <p>Italy</p>
-                       </div>
+                                    <p>Used vehicle</p>
+                                </div>
 
-                       <div class="col-sm-6">
-                            <label> Model</label>
-                       
-                            <p>Lamborghini</p>
-                       </div>
+                                <div class="col-sm-6">
+                                    <label>Make</label>
 
-                       <div class="col-sm-6">
-                            <label>First registration</label>
-                       
-                            <p>05/2010</p>
-                       </div>
+                                    <p>Italy</p>
+                                </div>
 
-                       <div class="col-sm-6">
-                            <label>Mileage</label>
-                       
-                            <p>5000 km</p>
-                       </div>
+                                <div class="col-sm-6">
+                                    <label> Model</label>
 
-                       <div class="col-sm-6">
-                            <label>Fuel</label>
-                       
-                            <p>Diesel</p>
-                       </div>
+                                    <p>Lamborghini</p>
+                                </div>
 
-                       <div class="col-sm-6">
-                            <label>Engine size</label>
-                       
-                            <p>1800 cc</p>
-                       </div>
+                                <div class="col-sm-6">
+                                    <label>First registration</label>
 
-                       <div class="col-sm-6">
-                            <label>Power</label>
-                       
-                            <p>85 hp</p>
-                       </div>
+                                    <p>05/2010</p>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <label>Mileage</label>
+
+                                    <p>5000 km</p>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <label>Fuel</label>
+
+                                    <p>Diesel</p>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <label>Engine size</label>
+
+                                    <p>1800 cc</p>
+                                </div>
+
+                                <div class="col-sm-6">
+                                    <label>Power</label>
+
+                                    <p>85 hp</p>
+                                </div>
 
 
-                       <div class="col-sm-6">
-                            <label>Gearbox</label>
-                       
-                            <p>Manual</p>
-                       </div>
+                                <div class="col-sm-6">
+                                    <label>Gearbox</label>
 
-                       <div class="col-sm-6">
-                            <label>Number of seats</label>
-                       
-                            <p>4</p>
-                       </div>
+                                    <p>Manual</p>
+                                </div>
 
-                       <div class="col-sm-6">
-                            <label>Doors</label>
-                       
-                            <p>2/3</p>
-                       </div>
+                                <div class="col-sm-6">
+                                    <label>Number of seats</label>
 
-                       <div class="col-sm-6">
-                            <label>Color</label>
-                       
-                            <p>Black</p>
-                       </div>
-                    </div>
-                  </article>
-                  <article  class="tab-pane fade" id="v-pills-deux" role="tabpanel" aria-labelledby="v-pills-deux-tab">
-                    <h4>Vehicle Description</h4>
-                    
-                    <p>- Colour coded bumpers <br> - Tinted glass <br> - Immobiliser <br> - Central locking - remote <br> - Passenger airbag <br> - Electric windows <br> - Rear head rests <br> - Radio <br> - CD player <br> - Ideal first car <br> - Warranty <br> - High level brake light <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco                         laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat                     cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
-                   </article>
-                  <article  class="tab-pane fade" id="v-pills-trois" role="tabpanel" aria-labelledby="v-pills-trois-tab">
-                    <h4>Vehicle Extras</h4>
+                                    <p>4</p>
+                                </div>
 
-                    <div class="row">   
-                        <div class="col-sm-6">
-                            <p>ABS</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p>Leather seats</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p>Power Assisted Steering</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p>Electric heated seats</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p>New HU and AU</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <p>Xenon headlights</p>
-                        </div>
-                    </div>
-                  </article>
-                  <article  class="tab-pane fade" id="v-pills-quatre" role="tabpanel" aria-labelledby="v-pills-quatre-tab">
-                    <h4>Contact Details</h4>
+                                <div class="col-sm-6">
+                                    <label>Doors</label>
 
-                    <div class="row">   
-                        <div class="col-sm-6">
-                            <label>Name</label>
+                                    <p>2/3</p>
+                                </div>
 
-                            <p>John Smith</p>
-                        </div>
-                        <div class="col-sm-6">
-                            <label>Phone</label>
+                                <div class="col-sm-6">
+                                    <label>Color</label>
 
-                            <p>123-456-789 </p>
-                        </div>
-                        <div class="col-sm-6">
-                            <label>Mobile phone</label>
-                            <p>456789123 </p>
-                        </div>
-                        <div class="col-sm-6">
-                            <label>Email</label>
-                            <p><a href="#">john@carsales.com</a></p>
-                        </div>
-                    </div>
-                  </article>
-                </section>
-              </div>
+                                    <p>Black</p>
+                                </div>
+                            </div>
+                        </article>
+                        <article class="tab-pane fade" id="v-pills-deux" role="tabpanel"
+                                 aria-labelledby="v-pills-deux-tab">
+                            <h4>Vehicle Description</h4>
+
+                            <p>- Colour coded bumpers <br> - Tinted glass <br> - Immobiliser <br> - Central locking -
+                                remote <br> - Passenger airbag <br> - Electric windows <br> - Rear head rests <br> -
+                                Radio <br> - CD player <br> - Ideal first car <br> - Warranty <br> - High level brake
+                                light <br> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                                nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+                                deserunt mollit anim id est laborum.</p>
+                        </article>
+                        <article class="tab-pane fade" id="v-pills-trois" role="tabpanel"
+                                 aria-labelledby="v-pills-trois-tab">
+                            <h4>Vehicle Extras</h4>
+
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <p>ABS</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p>Leather seats</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p>Power Assisted Steering</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p>Electric heated seats</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p>New HU and AU</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <p>Xenon headlights</p>
+                                </div>
+                            </div>
+                        </article>
+                        <article class="tab-pane fade" id="v-pills-quatre" role="tabpanel"
+                                 aria-labelledby="v-pills-quatre-tab">
+                            <h4>Contact Details</h4>
+
+                            <div class="row">
+                                <div class="col-sm-6">
+                                    <label>Name</label>
+
+                                    <p>John Smith</p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <label>Phone</label>
+
+                                    <p>123-456-789 </p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <label>Mobile phone</label>
+                                    <p>456789123 </p>
+                                </div>
+                                <div class="col-sm-6">
+                                    <label>Email</label>
+                                    <p><a href="#">john@carsales.com</a></p>
+                                </div>
+                            </div>
+                        </article>
+                    </section>
+                </div>
             </div>
         </div>
     </section>
