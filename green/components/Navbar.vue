044183c42cd2331e4c28cae1ea896eb940175f10
Théo Sigaud <sigaudtheo@gmail.com>
2023-04-14 01:53:12 +0200

    Navbar

--- green/components/Navbar.vue
+++ green/components/Navbar.vue
@@ -3,7 +3,7 @@
         <NuxtLink to="/" class="active">Home</NuxtLink>
         <NuxtLink to="/cars" class="active">Cars</NuxtLink>
         <span class="iconnav" @click="myFunction">
-            <nuxt-img src="/bars-solid.svg" alt="menu" width="25px" />
+            <nuxt-img src="/bars-solid.svg" alt="menu" width="25px" style="filter: brightness(0) invert(1);" />
         </span>
     </div>
 </template>
